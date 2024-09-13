0d91b683b3066565fae25e5bd767639d8eb61da1
Joulerk <madventcher@gmail.com>
2024-09-13 18:56:15 +0300

    - add: requirements

--- config.py
+++ config.py
@@ -7,11 +7,10 @@ load_dotenv()
 class Config:
     TELEGRAM_ID = os.getenv('TELEGRAM_ID')
     TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
-
     DB_HOST = os.getenv('DB_HOST')
     DB_PORT = os.getenv('DB_PORT')
     DB_NAME = os.getenv('DB_NAME')
     DB_USER = os.getenv('DB_USER')
     DB_PASSWORD = os.getenv('DB_PASSWORD')
-
     WG_PASSWORD = os.getenv('WG_PASSWORD')
+
